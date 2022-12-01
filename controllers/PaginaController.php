40bf66b2f774b0d2c5a730173647486d32ad7312
Victor Smaniotto <victorsmaniotto@hotmail.com>
2022-11-30 22:32:06 -0300

    Finalizando o cadastro das páginas

---
+++ controllers/PaginaController.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT']."/helpers/Config.php";
+
+require_once $_SERVER['DOCUMENT_ROOT']."/models/Pagina.php";
+
+function index(){
+    $pagina = listarPaginas();
+    return $pagina;
+}
+
+function visualizar($id){
+    $pagina = buscarPagina($id);
+    return $pagina;
+}
+
+function cadastrar(){
+    $pagina = [];
+
+    if(!empty($_POST)){                         #este if verifica se os valores do POST NÃO estão vazios
+        $pagina['titulo'] = $_POST['titulo'];    #Caso não estejam, o vetor pagina vai receber os dados presentes no POST
+        $pagina['slug'] = $_POST['slug'];
+        $pagina['descricao'] = $_POST['descricao'];
+
+        if(cadastrarPagina($pagina)){   #se executar essa função, retornamos para o index
+            header("Location:/admin/pagina");
+            exit;
+            
+        }
+    }
+    return $pagina;
+}
+
+
+?>
